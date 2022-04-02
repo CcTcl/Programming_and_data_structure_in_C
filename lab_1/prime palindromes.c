effd24d622b619a93817eeefedff1191da71235a
CcTcl <91953392+CcTcl@users.noreply.github.com>
2022-04-02 23:34:27 +0800

    Update prime palindromes.c

--- lab_1/prime palindromes.c
+++ lab_1/prime palindromes.c
@@ -17,69 +17,69 @@ int main(){
 	}
 
 	for (int d1 = 1; d1 <= 9; d1+=2) {
-    	int pal;
-    	pal=d1*10+d1;
-    	if(isPrime(pal)&&pal>=a&&pal<=b) printf("%d\n",pal);
-	}
-
-	for (int d1 = 1; d1 <= 9; d1+=2) {
-    	for (int d2 = 0; d2 <= 9; d2++) {
     		int pal;
-    		pal=d1*100+d2*10+d1;
+    		pal=d1*10+d1;
     		if(isPrime(pal)&&pal>=a&&pal<=b) printf("%d\n",pal);
-    	}
 	}
 
 	for (int d1 = 1; d1 <= 9; d1+=2) {
-    	for (int d2 = 0; d2 <= 9; d2++) {
-    		int pal;
-    		pal=d1*1000+d2*100+d2*10+d1;
-    		if(isPrime(pal)&&pal>=a&&pal<=b) printf("%d\n",pal);
-    	}
+    		for (int d2 = 0; d2 <= 9; d2++) {
+    			int pal;
+    			pal=d1*100+d2*10+d1;
+    			if(isPrime(pal)&&pal>=a&&pal<=b) printf("%d\n",pal);
+    		}
 	}
 
 	for (int d1 = 1; d1 <= 9; d1+=2) {
-     	for (int d2 = 0; d2 <= 9; d2++) {
-         	for (int d3 = 0; d3 <= 9; d3++) {
-         		int pal;
-           		pal = 10000*d1 + 1000*d2 +100*d3 + 10*d2 + d1;
-           		if(isPrime(pal)&&pal>=a&&pal<=b) printf("%d\n",pal);
-         	}
-     	}
- 	}
+    		for (int d2 = 0; d2 <= 9; d2++) {
+    			int pal;
+    			pal=d1*1000+d2*100+d2*10+d1;
+    			if(isPrime(pal)&&pal>=a&&pal<=b) printf("%d\n",pal);
+    		}
+	}
 
 	for (int d1 = 1; d1 <= 9; d1+=2) {
-     	for (int d2 = 0; d2 <= 9; d2++) {
-         	for (int d3 = 0; d3 <= 9; d3++) {
-         		int pal;
-           		pal = 100000*d1 + 10000*d2 +1000*d3 + 100*d3 + 10*d2 + d1;
-           		if(isPrime(pal)&&pal>=a&&pal<=b) printf("%d\n",pal);
-         	}
-     	}
- 	}
-
-	for (int d1 = 1; d1 <= 9; d1+=2) {
-     	for (int d2 = 0; d2 <= 9; d2++) {
-         	for (int d3 = 0; d3 <= 9; d3++) {
-         		for (int d4=0;d4<=9;d4++){
+     		for (int d2 = 0; d2 <= 9; d2++) {
+         		for (int d3 = 0; d3 <= 9; d3++) {
          			int pal;
-           			pal = 1000000*d1 + 100000*d2 +10000*d3 +1000*d4+ 100*d3 + 10*d2 + d1;
+           			pal = 10000*d1 + 1000*d2 +100*d3 + 10*d2 + d1;
            			if(isPrime(pal)&&pal>=a&&pal<=b) printf("%d\n",pal);
-				 }
-         	}
-     	}
+         		}
+     		}
  	}
 
 	for (int d1 = 1; d1 <= 9; d1+=2) {
-     	for (int d2 = 0; d2 <= 9; d2++) {
-         	for (int d3 = 0; d3 <= 9; d3++) {
-         		for (int d4=0;d4<=9;d4++){
+     		for (int d2 = 0; d2 <= 9; d2++) {
+         		for (int d3 = 0; d3 <= 9; d3++) {
          			int pal;
-           			pal = 10000000*d1 + 1000000*d2 +100000*d3 +10000*d4+1000*d4+ 100*d3 + 10*d2 + d1;
+           			pal = 100000*d1 + 10000*d2 +1000*d3 + 100*d3 + 10*d2 + d1;
            			if(isPrime(pal)&&pal>=a&&pal<=b) printf("%d\n",pal);
+         		}
+     		}
+ 	}
+
+	for (int d1 = 1; d1 <= 9; d1+=2) {
+     		for (int d2 = 0; d2 <= 9; d2++) {
+         		for (int d3 = 0; d3 <= 9; d3++) {
+         			for (int d4=0;d4<=9;d4++){
+         				int pal;
+           				pal = 1000000*d1 + 100000*d2 +10000*d3 +1000*d4+ 100*d3 + 10*d2 + d1;
+           				if(isPrime(pal)&&pal>=a&&pal<=b) printf("%d\n",pal);
+				 	}
+         		}
+     		}
+ 	}
+
+	for (int d1 = 1; d1 <= 9; d1+=2) {
+     		for (int d2 = 0; d2 <= 9; d2++) {
+         		for (int d3 = 0; d3 <= 9; d3++) {
+         			for (int d4=0;d4<=9;d4++){
+         				int pal;
+           				pal = 10000000*d1 + 1000000*d2 +100000*d3 +10000*d4+1000*d4+ 100*d3 + 10*d2 + d1;
+           				if(isPrime(pal)&&pal>=a&&pal<=b) printf("%d\n",pal);
 				 }
-         	}
-     	}
+         		}
+     		}
  	}
 	return 0;
 }
